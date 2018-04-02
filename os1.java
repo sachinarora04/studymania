0675c4457a4cb47ad8571896e4457648f9f45405
sachinarora04 <37793939+sachinarora04@users.noreply.github.com>
2018-04-02 16:17:57 +0530

    Add files via upload

---
+++ os1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class TestClass {
+        
+    public static void main(String args[]) {
+            Scanner s = new Scanner(System.in);
+ 
+          int wtime[],btime[],rtime[],num,quantum,total;
+ 
+          wtime = new int[10];
+          btime = new int[10];
+          rtime = new int[10];
+ 
